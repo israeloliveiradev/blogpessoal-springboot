f7505c2c2a857ff638c0c4dd84f59fc3f3070ef3
Israel Oliveira <israeloliveiracontact@gmail.com>
2024-07-16 09:14:37 -0300

    Reapply ":sparkles: feat: sCadd CRUD spring Security - Usuario Login"
    This reverts commit a8d0c7b30c4aadf054cbca323bbc1fe5fc36b1d0.

--- src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Tema;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 	
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
