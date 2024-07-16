a8d0c7b30c4aadf054cbca323bbc1fe5fc36b1d0
Israel Oliveira <israeloliveiracontact@gmail.com>
2024-07-16 09:12:25 -0300

    Revert ":sparkles: feat: sCadd CRUD spring Security - Usuario Login"
    This reverts commit a0ee32afaed4d1c63a6257a335cae59bebe5607b.

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -36,10 +36,6 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-
-	@ManyToOne
-	@JsonIgnoreProperties("usuario")
-	private Usuario usuario;
 	
 	public String getTexto() {
 		return texto;
@@ -84,11 +80,4 @@ public class Postagem {
 		this.tema = tema;
 	}
 
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
 }
